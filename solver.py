bc3807d0838349940192e9532fedc5aa73c5fce5
Rhythm-Gupta <rhythm18082@iiitd.ac.in>
2020-03-30 22:10:34 +0530

    Boilerplate added

---
+++ solver.py
@@ -0,0 +1,21 @@
+import rubik
+
+def shortest_path(start, end):
+    """
+    For Question 1, using BFS, finds the shortest path from start_position to
+    end_position. Returns a list of moves.
+
+    You can use the rubik.quarter_twists move set.
+    Each move can be applied using rubik.perm_apply
+    """
+    raise NotImplementedError
+
+def shortest_path_optmized(start, end):
+    """
+    For Question 2, using 2-way BFS, finds the shortest path from start_position to
+    end_position. Returns a list of moves. 
+
+    You can use the rubik.quarter_twists move set.
+    Each move can be applied using rubik.perm_apply
+    """
+    raise NotImplementedError
